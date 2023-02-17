9f97527fd6a09e6cda5181d38c43fb13d9602ca7
George Emetu <g.emetu11@gmail.com>
2023-02-17 13:48:58 -0800

    it is well

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point for code
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc = 'z'; lc++)
+	{
+		if (lc != 'q' && lc != 'q')
+			putchar(lc);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
