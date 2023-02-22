8c3925b42627de1be6e5d270e9365f0407b63ed7
George Emetu <g.emetu11@gmail.com>
2023-02-22 14:36:19 -0800

    start

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char pc[] = "_putchar\n";
+	int i;
+
+	for (i = 0; i <= 8; i++)
+	{
+		_putchar (pc[i]);
+	}
+
+	return (0);
+}
