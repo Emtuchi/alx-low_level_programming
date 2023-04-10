9cd4ac3c9b4eb275ea88d718d7eef71461375a6a
George Emetu <g.emetu11@gmail.com>
2023-04-10 15:05:44 -0700

    i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t file, r, w;
 	char *buffer;
 
-	buffer = malloc(sizeof(char) * 10);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		return (0);
